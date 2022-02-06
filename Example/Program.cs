7564b1707a80c45c8e98321209582f53a39eed12
Brian Retamar <leo1632@hotmail.com>
2022-02-06 02:53:47 -0300

    Added example project

---
+++ Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CacheLibrary.Utility;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = "Example";
+            var hash = new Hash().GenerateMd5HashFromKey(source);
+
+            Console.WriteLine($"The MD5 hash of '{source}' is: {hash}");
+        }
+    }
+}
